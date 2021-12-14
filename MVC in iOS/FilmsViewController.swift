f0ffe20b18cd8dfacb009e89d0566b9ea7cee8c2
Doaa Albishri <Duaa_ali@outlook.sa>
2021-12-14 11:52:51 +0000

    Initial Commit

---
+++ MVC in iOS/FilmsViewController.swift
@@ -0,0 +1,59 @@
+//
+//  FilmsViewController.swift
+//  MVC in iOS
+//
+//  Created by admin on 14/12/2021.
+//
+
+import UIKit
+
+class FilmsViewController: UITableViewController {
+    // Hardcoded data for now
+          var films = [String]()
+         
+       override func viewDidLoad() {
+               super.viewDidLoad()
+           StarWarsModel.getAllFilms(completionHandler: { // passing what becomes "completionHandler" in the 'getAllPeople' function definition in StarWarsModel.swift
+                      data, response, error in
+                          do {
+                              // Try converting the JSON object to "Foundation Types" (NSDictionary, NSArray, NSString, etc.)
+                              if let jsonResult = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary {
+                                  if let results = jsonResult["results"] as? NSArray {
+                                      for film in results {
+                                          let filmDict = film as! NSDictionary
+                                          self.films.append(filmDict["title"]! as! String)
+                                      }
+                                  }
+                              }
+                              DispatchQueue.main.async {
+                                  self.tableView.reloadData()
+                              }
+                          } catch {
+                              print("Something went wrong")
+                          }
+                  })
+       }
+
+
+          override func didReceiveMemoryWarning() {
+              super.didReceiveMemoryWarning()
+          }
+          override func numberOfSections(in tableView: UITableView) -> Int {
+              // if we return - sections we won't have any sections to put our rows in
+              return 1
+          }
+          override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+              // return the count of people in our data array
+              return films.count
+          }
+          override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+              // Create a generic cell
+              let cell = UITableViewCell()
+              // set the default cell label to the corresponding element in the people array
+              cell.textLabel?.text = films[indexPath.row]
+              // return the cell so that it can be rendered
+              return cell
+          }
+
+
+}
